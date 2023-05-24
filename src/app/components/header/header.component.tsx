0c0f64531970321d51dfa5fd9dcd85446cde9489
Denny Juwono <djuwono@woolworths.com.au>
2023-05-24 14:49:20 +1000

    link to cart and login page

--- src/app/components/header/header.component.tsx
+++ src/app/components/header/header.component.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './header.component.module.scss';
 
 const Header: React.FC = () => {
   return (
     <header className={styles['header']}>
       <div className="flex flex-row">
-        <Image
-          // className={styles.logo}
-          src="/logo.jpg"
-          alt="logo"
-          width={80}
-          height={80}
-          priority
-        />
+        <Link href="/">
+          <Image
+            // className={styles.logo}
+            src="/logo.jpg"
+            alt="logo"
+            width={80}
+            height={80}
+            priority
+          />
+        </Link>
         <div className="flex-grow">search</div>
-        <div>login/register</div>
-        <div>cart</div>
+        <div>
+          <Link href="/register">Login or sign up</Link>
+        </div>
+        <div>
+          <Link href="/cart">Cart</Link>
+        </div>
       </div>
       <nav>
         <ul className="flex flex-row">
